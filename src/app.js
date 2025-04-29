2065d6aa1cb718aa6fefb841e8f8588013b47664
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-04-29 10:06:50 +0200

    add normalize and other generic code 'root'

--- src/app.js
+++ src/app.js
@@ -1,44 +1,3 @@
-// var pf = new petfinder.Client({
-// 	apiKey: "Z9dKanxKljx0qyg74NrdG0JWvmV0z1XKEzbqEBfibH06EPh8hC",
-// 	secret: "QVxf7S7BZa0qAFtzslN4dYOVOFzaYteZHFKSpyDJ",
-// });
-
-// let animals = [];
-
-// pf.animal
-// 	.search({type: "cat"})
-// 	.then(function (response) {
-// 		console.log(response);
-// 		animals = response.data.animals
-// 		// Do something with `response.data.animals`
-// 	})
-// 	.catch(function (error) {
-// 	});
-
-const pf = new petfinder.Client({
-	apiKey: "Z9dKanxKljx0qyg74NrdG0JWvmV0z1XKEzbqEBfibH06EPh8hC",
-	secret: "QVxf7S7BZa0qAFtzslN4dYOVOFzaYteZHFKSpyDJ",
-});
-
-const tryingstuff = async (animal) => {
-	const response = await pf.animal.search({ type: animal.trim() });
-	const data = response.data.animals[0];
-	console.log("Esto vale el fetch actualizado", response);
-	console.log("esto vale data", data);
-};
-
-// tryingstuff("cat");
-
-// revisar video RICKY MORTY (clase 22 asyncronia - promesa - peticiones)
-// const waitForAnswer = async () => {
-// 	response = await pf.animal.search({type: "cat"}).then(function (response) {
-// 		return response;
-// 	})
-// 	animals = await response.data.animals;
-// 	console.log("esto vale response", animals);
-// }
-
-// waitForAnswer();
 
 const sendingAFetch = async () => {
 	try {
@@ -57,3 +16,5 @@ const sendingAFetch = async () => {
 };
 
 sendingAFetch();
+
+console.log("hola");
