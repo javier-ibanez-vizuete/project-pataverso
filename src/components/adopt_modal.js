7c2f9c4336891d4f678f8397f58ff84adf61474c
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-05-09 13:06:51 +0200

    add styles on adopt form modal

--- src/components/adopt_modal.js
+++ src/components/adopt_modal.js
@@ -1,3 +1,5 @@
+
+
 export const createAdoptModal = (animalName) => {
 	const body = document.querySelector("body");
 
@@ -24,11 +26,38 @@ export const createAdoptModal = (animalName) => {
             </select>
         </div>
         <div class="input-adopt-container">
-        
+            <label for="select-adopt-form-home-alone">¿Cuántas horas suele estar la vivienda vacía?</label>
+            <select id="select-adopt-form-home-alone" name="select-adopt-form-home-alone" required>
+                <option value="disabled" disabled selected>Elige una opción</option>
+                <option value="always">Más de 4 horas</option>
+                <option value="sometimes">Entre 2 y 4 horas</option>
+                <option value="almost-never">Menos de 2 horas</option>
+            </select>
+        </div>
+        <div class="input-adopt-container">
+            <label for="select-adopt-form-handle-vet-bills">¿Puedes afrontar gastos veterinarios básicos(vacunas, alimentacion, etc.)?</label>
+            <select id="select-adopt-form-handle-vet-bills" name="select-adopt-form-handle-vet-bills" required>
+                <option value="disabled" disabled selected>Elige una opción</option>
+                <option value="yes">Si</option>
+                <option value="no">No</option>
+            </select>
+        </div>
+        <div class="input-adopt-container">
+            <label for="select-adopt-form-more-pets">¿Tienes otras mascotas actualmente?</label>
+            <select id="select-adopt-form-more-pets" name="select-adopt-form-more-pets" required>
+                <option value="disabled" disabled selected>Elige una opción</option>
+                <option value="yes">Si</option>
+                <option value="no">No</option>
+            </select>
+        </div>
+        <div class="btns-adopt-form-container">
+            <button class="btn-apply-adopt-form btn-style">ENVIAR FORMULARIO</button>
+            <button class="btn-reject-adopt-form btn-style">CANCELAR FORMULARIO</button>
         </div>
     </form>
     `;
 
+    handleAdoptForm();
 	bgModal.append(modal);
 
 	body.append(bgModal);
