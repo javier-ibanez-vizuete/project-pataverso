626ce9bc7f245ed1ef9905db9eac3b28e5d2bd4c
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-05-09 14:13:05 +0200

    add response function for adopt form

--- src/components/adopt_modal.js
+++ src/components/adopt_modal.js
@@ -1,4 +1,91 @@
+import { saveDataInStorage } from "../helpers/storage.js";
 
+const calculateResponse = (animalName) => new Promise((resolve, reject) => {
+		const inputHomeType = document.querySelector("#select-adopt-form-home-type");
+		const inputHomeAlone = document.querySelector("#select-adopt-form-home-alone");
+		const inputHandleBills = document.querySelector("#select-adopt-form-handle-vet-bills");
+		const inputMorePets = document.querySelector("#select-adopt-form-more-pets");
+    let counter = 0
+
+    if (inputHomeType.value === "flat") {
+        counter += 1;
+    }
+    if (inputHomeType.value === "house") {
+        counter += 2;
+    }
+    if (inputHomeType.value === "country-state") {
+        counter += 3;
+    }
+    if (inputHomeAlone.value === "sometimes") {
+        counter += 1;
+    }
+    if (inputHomeAlone.value === "almost-never") {
+        counter += 2;
+    }
+    if (inputMorePets.value === "yes") {
+        counter += 1;
+    }
+    if (inputHandleBills.value === "no") {
+        counter = 0;
+    }
+    if (inputHandleBills.value === "yes") {
+        counter += 1;
+    }
+    if (counter < 1) {
+        reject("No esta capacitado para la adopcion a traves de Pataverso. Disculpe las molestias");
+        return
+    }
+    if (counter >= 1 && counter < 7) {
+        resolve(`Le hemos añadido a la lista de candidatos para la adopcion de ${animalName}.
+            Nos pondremos en contacto con usted.
+            `)
+    }
+    if (counter >= 7) {
+        resolve("Nos ha encantado el resultado de su formulario. Nos pondremos en contacto con usted lo antes posible")
+    }
+})
+
+const handleAdoptForm = async (animalName) => {
+	const form = document.querySelector(".adopt-form-contaier");
+
+	form.addEventListener("submit", async (event) => {
+		const inputHomeType = document.querySelector("#select-adopt-form-home-type");
+		const inputHomeAlone = document.querySelector("#select-adopt-form-home-alone");
+		const inputHandleBills = document.querySelector("#select-adopt-form-handle-vet-bills");
+		const inputMorePets = document.querySelector("#select-adopt-form-more-pets");
+		event.preventDefault();
+
+		if (inputHomeType.value === "disabled") {
+			alert("Por favor elija un tipo de casa");
+			inputHomeType.scrollIntoView({ behavior: "smooth", block: "center" });
+			return;
+		}
+		if (inputHomeAlone.value === "disabled") {
+			alert("Necesita elegir cuanto tiempo esta la casa vacia.");
+			inputHomeAlone.scrollIntoView({ behavior: "smooth", block: "center" });
+			return;
+		}
+		if (inputHandleBills.value === "disabled") {
+			alert("Por favor debe rellenar el campo - Afrontar gastos veterinarios");
+			inputHandleBills.scrollIntoView({ behavior: "smooth", block: "center" });
+			return;
+		}
+		if (inputMorePets.value === "disabled") {
+			alert("Debe elegir si tiene más mascotas en casa");
+			inputMorePets.scrollIntoView({ behavior: "smooth", block: "center" });
+			return;
+		}
+        try {
+        const response = await calculateResponse(animalName);
+		alert(response);
+		window.location.href = "/index.html";
+        } catch (error) {
+            alert(error);
+            saveDataInStorage("sesionIsOpen", false);
+            window.location.reload();
+        }
+	});
+};
 
 export const createAdoptModal = (animalName) => {
 	const body = document.querySelector("body");
@@ -57,8 +144,7 @@ export const createAdoptModal = (animalName) => {
     </form>
     `;
 
-    handleAdoptForm();
 	bgModal.append(modal);
-
 	body.append(bgModal);
+	handleAdoptForm(animalName);
 };
