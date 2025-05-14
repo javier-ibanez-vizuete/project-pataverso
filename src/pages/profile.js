ba97c62af48625f27f55a0a6b2abac7844e5c30f
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-05-14 20:17:06 +0200

    add sponsored animals to DOM from array of user

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,79 @@
 import { getDataFromStorage } from "../helpers/storage.js";
 
+const createAnimalInfoContainer = (animal) => {
+	const infoContainer = document.createElement("div");
+	infoContainer.classList.add("sponsored-animal-info-cotnainer");
+
+	const name = document.createElement("h4");
+	name.innerHTML = `NOMBRE: <span>${animal.nombre}</span>`;
+
+    const genre = document.createElement("h4");
+    genre.innerHTML = `SEXO: <span>${animal.genero}</span>`
+
+    const age = document.createElement("h4");
+    age.innerHTML = `EDAD: <span>${animal.edad}</span>`
+
+    const type = document.createElement("h4");
+    type.innerHTML = `TIPO DE MASCOTA: <span>${animal.tipo}</span>`
+
+	infoContainer.append(name, genre, age, type);
+
+	return infoContainer;
+};
+
+const createAnimalImage = (imagen) => {
+	console.log("que vale imagen", imagen);
+	const imageContainer = document.createElement("div");
+	imageContainer.classList.add("sponsored-animal-image-container");
+	const image = document.createElement("img");
+	console.log(("que vale image", image));
+	image.src = imagen;
+	imageContainer.appendChild(image);
+
+	return imageContainer;
+};
+
+const createAnimalCard = (animals) => {
+	const cardsContainer = document.querySelector(".sponsored-animals-cards-container");
+	if (!cardsContainer) {
+		return;
+	}
+	cardsContainer.innerHTML = "";
+
+	if (!animals.length) {
+		cardsContainer.innerHTML = `
+	        <h4 class="h4-no-pet-message">Actualmente no tienes ninguna mascota Apadrinada</h4>
+        `;
+	}
+	animals.forEach((animal) => {
+		const animalCard = document.createElement("div");
+		animalCard.classList.add("sponsor-animal-card");
+
+		const { imagen } = animal;
+		const image = createAnimalImage(imagen);
+		const info = createAnimalInfoContainer(animal);
+		animalCard.append(image, info);
+		cardsContainer.appendChild(animalCard);
+	});
+};
+
+const renderSponsoredAnimals = () => {
+	const currentUser = getDataFromStorage("currentUser");
+
+	const users = getDataFromStorage("usersData");
+	if (!currentUser) {
+		console.log("no se encontro currentUser");
+	}
+
+	const userIndex = users.findIndex((user) => user.id === currentUser.id);
+	if (userIndex === -1) {
+		console.log("No se encontro userIndex");
+	}
+
+	const animals = users[userIndex].sponsoring;
+	createAnimalCard(animals);
+};
+
 const sectionsAnimations = () => {
 	const sponsoredSection = document.querySelector(".section-sponsored-animals");
 	const userDetailsSection = document.querySelector(".section-user-details");
@@ -12,7 +86,7 @@ const sectionsAnimations = () => {
 		}
 		if (!sectionAnimalsCards) {
 			console.log("No se encuentró sectionAnimalsCards");
-            return
+			return;
 		}
 		sponsoredSection.classList.toggle("profile-section-opened");
 		sectionAnimalsCards.classList.toggle("sponsored-animals-cards-container-opened");
@@ -24,19 +98,19 @@ const sectionsAnimations = () => {
 		if (arrowIcons.length) {
 			arrowIcons.forEach((icon) => icon.classList.toggle("icon-arrow-container-opened"));
 		}
-        if (!sectionFormDetails) {
-            console.log("No se encontro sectionFormDetails");
-            return
-        }
-        userDetailsSection.classList.toggle("profile-section-opened");
-        sectionFormDetails.classList.toggle("form-user-details-opened");
+		if (!sectionFormDetails) {
+			console.log("No se encontro sectionFormDetails");
+			return;
+		}
+		userDetailsSection.classList.toggle("profile-section-opened");
+		sectionFormDetails.classList.toggle("form-user-details-opened");
 	});
 };
 
 document.addEventListener("DOMContentLoaded", () => {
 	sectionsAnimations();
+	renderSponsoredAnimals();
 
 	const nombre = document.getElementById("nombre");
-	console.log(nombre);
 	nombre.textContent = getDataFromStorage("currentUser").nombre;
 });
